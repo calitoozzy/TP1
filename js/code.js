2ff63ee3ce550a2334e4febe7960d065c8056970
Carlos Francisco <86804652+calitoozzy@users.noreply.github.com>
2022-04-25 15:30:58 -0300

    primer TP

---
+++ js/code.js
@@ -0,0 +1,6 @@
+
+console.log("El dia de mi cumpleaños es 3");
+console.log("El mes de mi nacimiento es el 7");
+console.log("El año en que naci 1977");
+console.log("Mi numero favorito es el 86") 
+
